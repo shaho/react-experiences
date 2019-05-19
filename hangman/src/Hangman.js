1f1431a0cde0c0f680cf695d43dd17136b930648
shaho <toofani@gmail.com>
2019-05-19 20:47:40 +0430

    Skeleton

---
+++ hangman/src/Hangman.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class Hangman extends Component {
+    static defaultProps = {
+        maxWrong: 6
+    };
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Hangman;
