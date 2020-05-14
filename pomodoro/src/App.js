0d1d3ad65830e4e6e60e53968e5c64f6cd2d02b2
Shaho <toofani@gmail.com>
2020-05-14 17:49:32 +0430

    Add and initialize Pomodoro

---
+++ pomodoro/src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function padTime(time) {
+  return time.toString().padStart(2, "0");
+}
+
+function App() {
+  const [timeLeft, setTimeLeft] = useState(25 * 60);
+
+  const minutes = padTime(Math.floor(timeLeft / 60));
+  const seconds = padTime(timeLeft - minutes * 60);
+
+  return (
+    <div className="app">
+      <h2>Pomodoro!</h2>
+
+      <div className="timer">
+        <span>{minutes}</span>
+        <span>:</span>
+        <span>{seconds}</span>
+      </div>
+
+      <div className="buttons">
+        <button>Start</button>
+        <button>Stop</button>
+        <button>Reset</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
