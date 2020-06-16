5291f8715a26d98174dbd0a1a7185d9d9a680e7f
Shaho <toofani@gmail.com>
2020-06-16 18:39:18 +0430

    Add dummy data to todo array

--- redux-toolkit-comparison/src/App.tsx
+++ redux-toolkit-comparison/src/App.tsx
@@ -8,24 +8,31 @@ import React, {
 import "./App.css";
 import { v1 as uuid } from "uuid";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const todos: Todo[] = [
+  {
+    id: uuid(),
+    desc: "Learn React",
+    isComplete: true,
+  },
+  {
+    id: uuid(),
+    desc: "Learn Redux",
+    isComplete: true,
+  },
+  {
+    id: uuid(),
+    desc: "Learn Redux-Toolkit",
+    isComplete: false,
+  },
+];
+
+const selectedTodoItem = todos[1].id;
+const editCount = 0;
+
+const App = () => {
+  const [newTodoInput, setNewTodoInput] = useState<string>("");
+  const [editTodoInput, setEditTodoInput] = useState<string>("");
+  const [isEditMode, setIsEditMode] = useState<boolean>(false);
+};
 
 export default App;
