b69bc231bd97e78b9199bab735610447490ccb67
Shaho <toofani@gmail.com>
2020-06-17 19:15:20 +0430

    Hook based component is completed

--- redux-toolkit-comparison/src/App.tsx
+++ redux-toolkit-comparison/src/App.tsx
@@ -6,6 +6,7 @@ import React, {
   useEffect,
 } from "react";
 import "./App.css";
+import { Todo } from "./type";
 import { v1 as uuid } from "uuid";
 
 const todos: Todo[] = [
@@ -26,7 +27,7 @@ const todos: Todo[] = [
   },
 ];
 
-const selectedTodoItem = todos[1].id;
+const selectedTodoId = todos[1].id;
 const editedCount = 0;
 
 const App = () => {
@@ -35,18 +36,61 @@ const App = () => {
   const [isEditMode, setIsEditMode] = useState<boolean>(false);
   const editInput = useRef<HTMLInputElement>(null);
 
-  const selectTodo =
-    (selectedTodoItem && todos.find((todo) => todo.id === selectedTodoItem)) ||
+  const selectedTodo =
+    (selectedTodoId && todos.find((todo) => todo.id === selectedTodoId)) ||
     null;
 
   const handleNewInputChange = (e: ChangeEvent<HTMLInputElement>): void => {
     setNewTodoInput(e.target.value);
   };
 
+  const handleEditInputChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    setEditTodoInput(e.target.value);
+  };
+
+  const handleCreateNewTodo = (e: FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+  };
+
+  const handleSelectTodo = (todoId: string) => (): void => {};
+
+  const handleEdit = (): void => {
+    if (!selectedTodo) return;
+
+    setEditTodoInput(selectedTodo.desc);
+    setIsEditMode(true);
+  };
+
+  useEffect(() => {
+    if (isEditMode) {
+      editInput?.current?.focus();
+    }
+  }, [isEditMode]);
+
+  const handleUpdate = (e: FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+  };
+
+  const handleCancelUpdate = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ): void => {
+    e.preventDefault();
+    setIsEditMode(false);
+    setEditTodoInput("");
+  };
+
+  const handleToggle = (): void => {
+    if (!selectedTodoId || !selectedTodo) return;
+  };
+
+  const handleDelete = (): void => {
+    if (!selectedTodoId) return;
+  };
+
   return (
     <div className="App">
-      <div className="App_counter">Todos Updated Count: {editedCount}</div>
-      <div className="App_header">
+      <div className="App__counter">Todos Updated Count: {editedCount}</div>
+      <div className="App__header">
         <h1>Todo: Redux vs RTK Edition</h1>
         <form onSubmit={handleCreateNewTodo}>
           <label htmlFor="new-todo">Add new:</label>
@@ -58,7 +102,6 @@ const App = () => {
           <button type="submit">Create</button>
         </form>
       </div>
-
       <div className="App__body">
         <ul className="App__list">
           <h2>My Todos:</h2>
@@ -74,6 +117,38 @@ const App = () => {
             </li>
           ))}
         </ul>
+        <div className="App_todo-info">
+          <h2>Selected Todo:</h2>
+          {selectedTodo === null ? (
+            <span className="empty-state">No Todo Selected</span>
+          ) : !isEditMode ? (
+            <>
+              <span
+                className={`todo-desc ${
+                  selectedTodo?.isComplete ? "done" : ""
+                }`}
+              >
+                {selectedTodo.desc}
+              </span>
+              <div className="todo-actions">
+                <button onClick={handleEdit}>Edit</button>
+                <button onClick={handleToggle}>Toggle</button>
+                <button onClick={handleDelete}>Delete</button>
+              </div>
+            </>
+          ) : (
+            <form onSubmit={handleUpdate}>
+              <label htmlFor="edit-todo">Edit:</label>
+              <input
+                ref={editInput}
+                onChange={handleEditInputChange}
+                value={editTodoInput}
+              />
+              <button type="submit">Update</button>
+              <button onClick={handleCancelUpdate}>Cancel</button>
+            </form>
+          )}
+        </div>
       </div>
     </div>
   );
