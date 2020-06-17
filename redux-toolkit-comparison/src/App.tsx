a537827b11676e044f88d424bbdeee7db45528d6
Shaho <toofani@gmail.com>
2020-06-17 17:31:49 +0430

    Add todo counter

--- redux-toolkit-comparison/src/App.tsx
+++ redux-toolkit-comparison/src/App.tsx
@@ -27,12 +27,56 @@ const todos: Todo[] = [
 ];
 
 const selectedTodoItem = todos[1].id;
-const editCount = 0;
+const editedCount = 0;
 
 const App = () => {
   const [newTodoInput, setNewTodoInput] = useState<string>("");
   const [editTodoInput, setEditTodoInput] = useState<string>("");
   const [isEditMode, setIsEditMode] = useState<boolean>(false);
+  const editInput = useRef<HTMLInputElement>(null);
+
+  const selectTodo =
+    (selectedTodoItem && todos.find((todo) => todo.id === selectedTodoItem)) ||
+    null;
+
+  const handleNewInputChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    setNewTodoInput(e.target.value);
+  };
+
+  return (
+    <div className="App">
+      <div className="App_counter">Todos Updated Count: {editedCount}</div>
+      <div className="App_header">
+        <h1>Todo: Redux vs RTK Edition</h1>
+        <form onSubmit={handleCreateNewTodo}>
+          <label htmlFor="new-todo">Add new:</label>
+          <input
+            onChange={handleNewInputChange}
+            id="new-todo"
+            value={newTodoInput}
+          />
+          <button type="submit">Create</button>
+        </form>
+      </div>
+
+      <div className="App__body">
+        <ul className="App__list">
+          <h2>My Todos:</h2>
+          {todos.map((todo, i) => (
+            <li
+              className={`${todo.isComplete ? "done" : ""} ${
+                todo.id === selectedTodoId ? "active" : ""
+              }`}
+              key={todo.id}
+              onClick={handleSelectTodo(todo.id)}
+            >
+              <span className="list-number">{i + 1})</span> {todo.desc}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default App;
